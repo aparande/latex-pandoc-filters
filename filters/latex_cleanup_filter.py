0e5dbea44151c468bcbfbc7185258ff59eac14e7
Anmol Parande <parandea1.7@gmail.com>
2021-12-30 19:21:53 +0530

    Fixed tabbing in LatexCleanupFilter

--- filters/latex_cleanup_filter.py
+++ filters/latex_cleanup_filter.py
@@ -6,6 +6,6 @@ from .pandoc_filter import PandocFilter
 class LatexCleanupFilter(PandocFilter):
   def __call__(self, key:str, value:Any, fmt:str, meta:Any):
     if key == "RawInline" or key =="RawBlock":
-        [fmt, code] = value
-        if fmt == "latex":
-							return []
+      [fmt, code] = value
+      if fmt == "latex":
+        return []
